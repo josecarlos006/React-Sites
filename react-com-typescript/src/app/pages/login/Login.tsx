868d9629e52b737bc2984dc304f3d30336106e9a
José Carlos <130001671+josecarlos006@users.noreply.github.com>
2024-06-04 16:49:58 -0300

    Update
    login form using react's useState

--- react-com-typescript/src/app/pages/login/Login.tsx
+++ react-com-typescript/src/app/pages/login/Login.tsx
@@ -1,16 +1,30 @@
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react"
 
 const Login = () => {
-  const navigate = useNavigate();
-  
-  const handleClick = () => {
-    navigate("/pagina-inicial");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEntrar = () => {
+    console.log(email);
+    console.log(password);
   }
 
   return (
     <div>
-      <h1>Login</h1>
-      <button onClick={handleClick}>Pagina Inicial</button>
+      <form>
+        <label>
+          <span>E-mail</span>
+          <input value={email} onChange={e => setEmail(e.target.value)} type="email" />
+        </label>
+        <label>
+          <span>Senha</span>
+          <input value={password} onChange={e => setPassword(e.target.value)} type="password" />
+        </label>
+
+        <button type="button" onClick={handleEntrar}>
+          Entrar
+        </button>
+      </form>
     </div>
   )
 }
